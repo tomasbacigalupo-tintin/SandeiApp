0746b5ebeb621811a6550189860a7a1ede6c5416
Tomás Bacigalupo <tbacigalupo.bya@gmail.com>
2025-05-12 23:09:50 +0000

    chore: limpieza del repo, eliminación de duplicados y renombrado de frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Login from "@/pages/Login"
+import Dashboard from "@/pages/Dashboard"
+import Register from "@/pages/Register"
+import PrivateRoute from "@/components/PrivateRoute"
+import Players from "@/pages/Players"
 
-const App: React.FC = () => (
-  <div>
-    <h1>Hello from React + TypeScript</h1>
-  </div>
-);
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+	<Route		path="/players"
+  element={
+    <PrivateRoute>
+      <Players />
+    </PrivateRoute>
+  }
+/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
-export default App;
+export default App
+
