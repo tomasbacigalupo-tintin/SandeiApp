3ce042be5a1b7306f93f638536926dac4a3d81c4
Tomas_Bacigalupo <69203584+tomasbacigalupo-tintin@users.noreply.github.com>
2025-05-28 16:12:09 -0300

    Enable DTO date transformation and update controller

--- backend/src/modules/matches/dto/update-match.dto.ts
+++ backend/src/modules/matches/dto/update-match.dto.ts
@@ -1,9 +1,11 @@
 import { PartialType } from "@nestjs/mapped-types";
 import { IsDateString, IsOptional } from "class-validator";
+import { Transform } from "class-transformer";
 import { CreateMatchDto } from "./create-match.dto";
 
 export class UpdateMatchDto extends PartialType(CreateMatchDto) {
   @IsOptional()
   @IsDateString()
-  date?: string;
+  @Transform(({ value }) => value ? new Date(value) : undefined)
+  date?: Date;
 }
