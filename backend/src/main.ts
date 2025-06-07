86c7b62b1e854b5baa63a056314e433ac8eb70a0
Tomas_Bacigalupo <69203584+tomasbacigalupo-tintin@users.noreply.github.com>
2025-06-07 12:05:21 -0300

    Improve security handling

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
 import { EntityNotFoundFilter } from './filters/entity-not-found.filter';
 import { env } from './config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.useGlobalFilters(new EntityNotFoundFilter());
   await app.listen(env.backendPort);
-  console.log(`🚀 Backend listening on port ${env.backendPort}`);
+  logger.log(`🚀 Backend listening on port ${env.backendPort}`);
 }
 bootstrap();
